16d107eece55a7263a7182e6f44d1acf927141c6
Siddhant Vij <siddhantvij201@gmail.com>
2024-03-26 06:50:34 +0530

    Go program - Not Implemented

--- Go/main.go
+++ Go/main.go
@@ -7,3 +7,9 @@ import (
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+// Can be done using the GoCV library at https://github.com/hybridgroup/gocv
+// - Setting up is a chore
+// - Already done with other languages: Java, C++, Python
+// - Functionality remains consistent in Go as well. Just another interface layer with the OpenCV library
+// - Will tackle this in the future (maybe)
